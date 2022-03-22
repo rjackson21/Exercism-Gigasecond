c83fffc3efcb2f9614be0e86d99b39ae299a4455
Regina Jackson <rjackson156@gmail.com>
2022-03-22 18:34:59 -0400

    solution

---
+++ gigasecond.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (time) => {
+  let date = time.getTime();
+  const gs = Math.pow(10, 9);
+
+  return new Date(date + gs * 1000)
+  //the *10000 changes gigasecond to milisecond 
+  };
